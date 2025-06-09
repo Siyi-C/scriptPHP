6183ab80624e2227c2d510f0af929c096f04e8b5
Siyi Chen <siychenyy@gmail.com>
2025-06-09 16:33:56 +1000

    updated user_upload.php file

--- user_upload.php
+++ user_upload.php
@@ -3,29 +3,23 @@
 require_once __DIR__ . '/Database.php';
 require_once __DIR__ . '/HandleCsv.php';
 
-$options = require __DIR__ . "/options.php";
-
-$shortOptions = "";
-$longOptions = [];
-
-foreach ($options as $opt) {
-    if(!empty($opt['short'])) {
-        $shortOptions .= $opt['short'];
-    }
-     if(!empty($opt['long'])) {
-        $longOptions[] = $opt['long'];
-    }
-}
-
-$opts = getopt($shortOptions, $longOptions);
+$opts = getopt("u:p:h:", ["file:", "create_table", "dry_run", "help"]);
 
 if (isset($opts['help'])){
-    echo "\nUsage: php user_upload.php [options]\n\n";
-    echo "Options:\n";
-    
-    foreach ($options as $opt) {
-        echo $opt['description'];
-    }
+ echo <<<EOD
+Usage: php user_upload.php [options]
+
+Options:
+    --file [csv file name]     this is the name of the CSV to be parsed
+    --create_table             this will cause the MySQL users table to be built (and no further action will be taken)
+    --dry_run                  this will be used with the --file directive in case we want to run the script but not insert
+                               into the DB. All other functions will be executed, but the database won't be altered"
+    -u [username]              MySQL username
+    -p [password]              MySQL password
+    -h [host]                  MySQL host
+    --help                     Which will output the above list of directives with details
+
+EOD;
     exit;
 }
 
@@ -44,6 +38,7 @@ if (isset($opts['file'])) {
       echo "Error: --file option requires a filename. Example: php user_upload.php --file [filename] \n";
       exit;
 }
+
 
 
 
