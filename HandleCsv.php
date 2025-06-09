bea5b21cfee10d5b7cce83cb9892cc0815d4cf14
Siyi Chen <siychenyy@gmail.com>
2025-06-09 16:39:30 +1000

    add insert data to database and validation

--- HandleCsv.php
+++ HandleCsv.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class HandleCsv {
 
     private PDO $db;
@@ -15,7 +14,6 @@ class HandleCsv {
     // create table
     public function createTable(): void
     {
-
         $sql = "
             CREATE TABLE IF NOT EXISTS users (
                 `id` INT AUTO_INCREMENT PRIMARY KEY,
@@ -37,23 +35,34 @@ class HandleCsv {
     //read csv
     public function readCsv(string $filename): array
     {
-
         if (!file_exists($filename)) {
             throw new InvalidArgumentException("File $filename does not exist.\n");
         }
 
         $rows = [];
+        $skippedRows = 0;
 
         if (($handle = fopen($filename, 'r')) !== false) {
             $header = fgetcsv($handle);
             if ($header === false) {
                  throw new RuntimeException("CSV header could not be read.\n");
             }
+
+            // remove space
+            $header = array_map('trim', $header);
             
             while(($row = fgetcsv($handle)) !== false) {
+                $row = array_map('trim', $row);
                 $data = array_combine($header, $row);
-                $rows[] = $data;
+
+           $parsed = $this->parseCsv($data);
+            if ($parsed === null) {
+                $skippedRows++;
+                continue;
             }
+
+            $rows[] = $parsed;
+        }
 
             fclose($handle);
 
@@ -67,7 +76,74 @@ class HandleCsv {
     //insert data to db
     public function insert(array $data): void
     {
-    
+       $prepareData = $this->db->prepare("
+            INSERT INTO users (name, surname, email) 
+            VALUES (:name, :surname, :email)
+        ");
+
+        if ($this->dryRun) {
+            $messages = [];
+            foreach ($data as $row) {
+                $messages[] = json_encode($row);
+        }
+        echo "[DRY RUN] Would insert " . count($data) . " rows:\n" . implode("\n", $messages) . "\n";
+        return;
+    }
+
+        foreach($data as $row) { 
+
+            // check email exits
+            if ($this->emailExists($row['email'])) {
+                echo "Found duplicate email:" . $row['email'] . "\n";
+                continue; 
+            }
+
+            try {
+                $prepareData->execute([
+                ':name' => $row['name'],
+                ':surname' => $row['surname'],
+                ':email' => $row['email'],
+            ]);
+        
+            } catch (PDOException $e) {
+                echo "Error insert data: " . $e->getMessage() . "\n";
+            }
+        }
+    }
+
+    private function parseCsv(array $data): ?array
+    {
+        // Check for required fields
+        if (!isset($data['name'], $data['surname'], $data['email'])) {
+            echo "Missing name, surname, or email: " . implode(',', $data) . "\n";
+            return null;
+        }
+
+        // Clean and format
+        $name = ucfirst(strtolower($data['name']));
+        $surname = ucfirst(strtolower($data['surname']));
+        $email = strtolower($data['email']);
+
+        // Validate email
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "Invalid email: " . $email . ". This user will not save to database.\n";
+            return null;
+        }
+        
+        return [
+            'name' => $name,
+            'surname' => $surname,
+            'email' => $email
+        ];
+    }   
+
+    private function emailExists(string $email): bool
+    {
+        $check = $this->db->prepare("
+            SELECT COUNT(*) FROM users where email = :email
+        ");
+        $check->execute([':email' => $email]);
+        return $check->fetchColumn() > 0;
     }
 
 }
